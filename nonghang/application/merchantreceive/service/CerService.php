06844c13bd1e40bd6a629bcf89685e4aeef0d966
zhumingliang <353575156@qq.com>
2020-12-16 10:34:23 +0800

    解决跨域问题

--- nonghang/application/merchantreceive/service/CerService.php
+++ nonghang/application/merchantreceive/service/CerService.php
@@ -9,11 +9,12 @@ class CerService
 
     public function save($cer)
     {
-        $path = dirname($_SERVER['SCRIPT_FILENAME']) . '/static/resources/certificate';
+      /*  $path = dirname($_SERVER['SCRIPT_FILENAME']) . '/static/resources/certificate';
         if (!is_dir($path)) {
             mkdir(iconv("UTF-8", "GBK", $path), 0777, true);
-        }
-        $info = $cer->move($path);
+        }*/
+       // $info = $cer->move($path);
+        $info= $cer->move('../static/resources/certificate/','');
         return ['url' => $info->getSaveName()];
     }
 }
